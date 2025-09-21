f0cdec54054682a7c00ebfdcf783b7f3586a7208
bhavnaamul <bhavna.amul@gmail.com>
2025-09-22 02:39:05 +0530

    first commit

---
+++ src/test/java/demo1/DemoDocker.java
@@ -0,0 +1,19 @@
+package demo1;
+
+public class DemoDocker {
+	
+	public static void main(String a[]) {
+		
+		System.out.println("DemoDocker passed 1");
+		
+		System.out.println("DemoDocker passed 2");
+		
+		System.out.println("DemoDocker passed 3");
+		
+		System.out.println("DemoDocker passed 4");
+		
+		System.out.println("DemoDocker passed 5");		
+		
+	}
+
+}
