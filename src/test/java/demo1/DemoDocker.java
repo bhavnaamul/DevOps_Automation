c041178cb92dd17b4ee18f00e00c7a874bb2d6f7
bhavnaamul <bhavna.amul@gmail.com>
2025-09-22 03:01:08 +0530

    updated file with dependencies

--- src/test/java/demo1/DemoDocker.java
+++ src/test/java/demo1/DemoDocker.java
@@ -1,19 +1,18 @@
 package demo1;
 
+import org.testng.annotations.Test;
+
 public class DemoDocker {
-	
 	public static void main(String a[]) {
-		
+
 		System.out.println("DemoDocker passed 1");
 		
 		System.out.println("DemoDocker passed 2");
-		
+
 		System.out.println("DemoDocker passed 3");
-		
+
 		System.out.println("DemoDocker passed 4");
-		
-		System.out.println("DemoDocker passed 5");		
-		
+
 	}
 
 }
